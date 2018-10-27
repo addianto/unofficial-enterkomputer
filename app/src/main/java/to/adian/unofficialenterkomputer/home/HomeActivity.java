3fbba94fedcf7925a922edea9d54b6d1c175444a
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-27 15:35:08 +0700

    Finish home screen MVP (#1)

--- app/src/main/java/to/adian/unofficialenterkomputer/home/HomeActivity.java
+++ app/src/main/java/to/adian/unofficialenterkomputer/home/HomeActivity.java
@@ -1,14 +1,24 @@
 package to.adian.unofficialenterkomputer.home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import to.adian.unofficialenterkomputer.R;
 import to.adian.unofficialenterkomputer.home.ui.home.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +28,59 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.nav_home:
+                        Log.i(TAG, "Home navigation was clicked");
+                        break;
+                    case R.id.nav_simulate:
+                        Log.i(TAG, "Simulate navigation was clicked");
+                        break;
+                    case R.id.nav_tracking:
+                        Log.i(TAG, "Tracking navigation was clicked");
+                        break;
+                    case R.id.nav_best_seller:
+                        Log.i(TAG, "Best seller navigation was clicked");
+                        break;
+                    default:
+                        Log.i(TAG, "A navigation item was clicked");
+                        break;
+                }
+
+                DrawerLayout drawer = findViewById(R.id.drawer);
+                drawer.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, HomeFragment.newInstance())
                     .commitNow();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer);
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }

6b22cccc35dc6d4e9b247678bb598989719be243
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-30 17:59:13 +0700

    Remove LiveData in Dao and use model class directly (#1)

--- app/src/main/java/to/adian/unofficialenterkomputer/data/CategoryDao.java
+++ app/src/main/java/to/adian/unofficialenterkomputer/data/CategoryDao.java
@@ -2,7 +2,6 @@ package to.adian.unofficialenterkomputer.data;
 
 import java.util.List;
 
-import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -12,10 +11,10 @@ import androidx.room.Query;
 public interface CategoryDao {
 
     @Query("SELECT * FROM categories ORDER BY name")
-    LiveData<List<Category>> getCategories();
+    List<Category> getCategories();
 
     @Query("SELECT * FROM categories WHERE id = :id")
-    LiveData<Category> getCategory(int id);
+    Category getCategory(int id);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(List<Category> categories);

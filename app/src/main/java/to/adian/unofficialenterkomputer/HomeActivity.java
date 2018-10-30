4050f14ada4c7e628f935fb795b6ca4fb364c9af
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-30 15:11:44 +0700

    Add button design for home screen (#1)

---
+++ app/src/main/java/to/adian/unofficialenterkomputer/HomeActivity.java
@@ -0,0 +1,30 @@
+package to.adian.unofficialenterkomputer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}

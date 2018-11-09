1f1702dcc2001b0bd724fe01b42333598c7c825f
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-11-09 20:44:50 +0700

    Add category model class (#4)

---
+++ app/src/main/java/to/adian/unofficialenterkomputer/model/Category.java
@@ -0,0 +1,32 @@
+package to.adian.unofficialenterkomputer.model;
+
+public class Category {
+
+    private final int id;
+    private final String name;
+    private final String endpoint;
+
+    public Category(int id, String name, String endpoint) {
+        this.id = id;
+        this.name = name;
+        this.endpoint = endpoint;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category(id=%d, name=%s, endpoint=%s)", id, name,
+                endpoint);
+    }
+}
